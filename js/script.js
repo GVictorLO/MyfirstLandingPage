2b8ed8d39d8297a8bb665053316c1852363f1b26
Gabriel Victor <gabrielvictor89@gmail.com>
2021-07-19 00:21:56 -0300

    mensagem

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let pesquisa = document.getElementById('pesquisa').value;
+    let data = {
+        nome,
+        pesquisa,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content')
+
+    let carregando = `<p>CARREGANDO...</p>`
+
+    let pronto = `<p>Obrigado por participar!</p>`
+
+    content.innerHTML = carregando
+
+
+    setTimeout(() => {
+        content.innerHTML = pronto
+    }, 1000)
+
+})
